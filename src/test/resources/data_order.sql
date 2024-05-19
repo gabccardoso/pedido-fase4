08113b47b8536233d2bfbbeab2c42c0a1b66a5cd
gabrielasousa <gabrielasousa@touchtec.com.br>
2024-05-19 15:21:34 -0300

    Microserviço pedido

---
+++ src/test/resources/data_order.sql
@@ -0,0 +1,5 @@
+INSERT INTO orders (client_id)
+VALUES (1);
+
+INSERT INTO product (id, name, category, description, image, price)
+VALUES (1001, 'HAMBURGUER', 'LANCHE', 'TOP TOP', 'image.png', 12.30);
